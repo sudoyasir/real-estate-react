ceccb01452de6b8cd85113a0ac19aae5ceaf9df6
Yasir Nawaz <94890956+yasir2002@users.noreply.github.com>
2023-08-10 04:45:23 +0500

    Footer completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Residencies from "./components/Residencies/Residencies";
 import Value from "./components/Value/Value";
 import Contact from "./components/Contact/Contact";
 import GetStarted from "./components/GetStarted/GetStarted";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
       <Value />
       <Contact />
       <GetStarted />
+      <Footer />
     </div>
   );
 }
