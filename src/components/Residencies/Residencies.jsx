d85866db0799a34566d8bf056795546c31047c64
Yasir Nawaz <94890956+yasir2002@users.noreply.github.com>
2023-08-09 23:43:23 +0500

    Residencies slider completed

--- src/components/Residencies/Residencies.jsx
+++ src/components/Residencies/Residencies.jsx
@@ -2,6 +2,8 @@ import "./Residencies.css";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import "swiper/css";
 import data from "../../utils/slider.json";
+import { sliderSettings } from "../../utils/common";
+
 const Residencies = () => {
   return (
     <section className="r-wrapper">
@@ -11,13 +13,14 @@ const Residencies = () => {
           <span className="primaryText">Popular Residencies</span>
         </div>
 
-        <Swiper>
+        <Swiper {...sliderSettings}>
+          <SliderButtons />
           {data.map((card, i) => (
             <SwiperSlide key={i}>
               <div className="flexColStart r-card">
                 <img src={card.image} alt="Home Image" />
 
-                <span className="secondayText r-price">
+                <span className="secondaryText r-price">
                   <span style={{ color: "orange" }}>$</span>
                   <span>{card.price}</span>
                 </span>
@@ -33,3 +36,13 @@ const Residencies = () => {
 };
 
 export default Residencies;
+
+const SliderButtons = () => {
+  const swiper = useSwiper();
+  return (
+    <div className="flexCenter r-buttons">
+      <button onClick={()=>swiper.slidePrev()}>&lt;</button>
+      <button onClick={()=>swiper.slideNext()}>&gt;</button>
+    </div>
+  );
+};
