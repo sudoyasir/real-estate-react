d0def0430c50f1d1a66fd56127e972520e76b435
Yasir Nawaz <94890956+yasir2002@users.noreply.github.com>
2023-08-10 03:08:17 +0500

    Our values section completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero/Hero";
 import './App.css'
 import Companies from "./components/Companies/Companies";
 import Residencies from "./components/Residencies/Residencies";
+import Value from "./components/Value/Value";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       </div>
       <Companies />
       <Residencies />
+      <Value />
     </div>
   );
 }
