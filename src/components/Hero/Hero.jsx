7c869e69d76253df3427e46e93f707c3739ae404
Yasir Nawaz <94890956+yasir2002@users.noreply.github.com>
2023-08-10 17:26:24 +0500

    Responsive + Header animations

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Hero.css";
 import { HiLocationMarker } from "react-icons/hi";
 import CountUp from "react-countup";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
@@ -12,9 +13,16 @@ const Hero = () => {
           <div className="flexColStart hero-left">
             <div className="hero-title">
               <div className="orange-circle"></div>
-              <h1>
+              <motion.h1
+                initial={{ y: "2rem", opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{
+                  duration: 2,
+                  type: "spring",
+                }}
+              >
                 Discover <br /> Most Suitable <br /> Property
-              </h1>
+              </motion.h1>
             </div>
 
             <div className="flexColStart hero-des">
@@ -60,11 +68,19 @@ const Hero = () => {
           </div>
 
           {/* Right Side */}
-          <div className="flexCenter hero-right">
+          <motion.div
+            initial={{ x: "7rem", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{
+              duration: 2.5,
+              type: "spring",
+            }}
+            className="flexCenter hero-right"
+          >
             <div className="image-container">
               <img src="./hero-image.png" alt="Hero Image" />
             </div>
-          </div>
+          </motion.div>
         </div>
       </section>
     </div>
